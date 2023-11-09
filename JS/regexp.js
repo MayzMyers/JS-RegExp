6541b8062f7ae75576ec31b5d4f66704cc8fa6be
MayzMyers <35607480+MayzMyers@users.noreply.github.com>
2023-11-09 19:06:42 +0300

    Добавление файла JS

---
+++ JS/regexp.js
@@ -0,0 +1,19 @@
+function task1() {
+  alert(`task1`);
+}
+
+function task2() {
+  alert(`task2`);
+}
+
+function task3() {
+  alert(`task3`);
+}
+
+function task4() {
+  alert(`task4`);
+}
+
+function task5() {
+  alert(`task5`);
+}
